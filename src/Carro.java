9f94a7abbb35720880eefb743a26e9703385f25b
LucasSaraiva1 <lucassaraivaxl@gmail.com>
2023-11-23 00:24:35 -0300

    Na Classe Carro foi feita a chamada de um método implementado pela Classe Motor

--- src/Carro.java
+++ src/Carro.java
@@ -1,16 +1,22 @@
 public class Carro {
     private boolean ligado;
     private int velocidade;
+    private Motor motor; // Adicionando um objeto Motor como atributo
 
     public Carro() {
         this.ligado = false;
         this.velocidade = 0;
+        this.motor = new Motor(); // Inicializando o objeto Motor
     }
 
     public void ligarCarro() {
         if (!ligado) {
+            System.out.println("");
+            System.out.println("-- EXECUÇÃO DA CLASSE CARRO.JAVA --");
+            System.out.println("");
             System.out.println("O carro foi ligado.");
             ligado = true;
+            motor.ligarMotor(); // Chamando o método ligarMotor da classe Motor
         } else {
             System.out.println("O carro já está ligado.");
         }
